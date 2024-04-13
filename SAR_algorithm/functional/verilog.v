7861f2d007892aa1bda681c6c07fcea7117812e9
PhillipRambo <93056982+PhillipRambo@users.noreply.github.com>
2024-04-13 13:26:15 +0200

    create SAR_algorithm modification

--- SAR_algorithm/functional/verilog.v
+++ SAR_algorithm/functional/verilog.v
@@ -30,37 +30,38 @@ module SAR_algorithm(
     output reg [7:0] D
 );
 
+
 reg [3:0] counter = 4'b0000;
 
-always @(posedge clk, rst) begin
+always @(posedge rst) begin
     if (rst) begin
-        // Reset logic
-        B <= 8'b0000000;
-        BN <= 8'b1111111;
+        B <= 7'b0000000;
+        BN <= 7'b0000000;
         D <= 8'b00000000;
         counter <= 4'b0000;
-    end else begin
-        if (En) begin
-            if (counter != 4'b1000) begin
-                // Bit searching logic
-                D[counter] <= Op;
-                
-                if (Op) begin
-                    B[counter] <= 1'b1;
-                    BN[counter] <= 1'b0;
-                end else if (Om) begin 
-                    B[counter] <= 1'b0;
-                    BN[counter] <= 1'b1;
-                end
-                
-                counter <= counter + 1'b1;         
+    end
+end
+
+always @(posedge clk) begin
+    if (En && !rst && (Op || Om)) begin  
+        if (counter != 4'b1000) begin
+            D[counter] <= Op;
+            
+            if (Op) begin
+                B[counter] <= 1'b1;
+                BN[counter] <= 1'b0;
+            end else if (Om) begin 
+                B[counter] <= 1'b0;
+                BN[counter] <= 1'b1;
             end
-        end else begin
-                B <= 8'b0000000;
-                BN <= 8'b1111111;
-                D <= 8'b00000000;
-                counter <= 4'b0000;
-            end
+            
+            counter <= counter + 1'b1;
+        end
+    end else if (!En && !rst) begin
+        B <= 7'b0000000;
+        BN <= 7'b000000;
+        D <= 8'b00000000;
+        counter <= 4'b0000;
     end
 end
 
